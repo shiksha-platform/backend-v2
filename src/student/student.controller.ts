f97f3960e7c980ee4a258de7beb302090ccdda49
ManojNathIC <manojnath164@gmail.com>
2022-03-25 11:15:10 +0530

    swagger UI

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Get, Post, Put, Patch, Param } from '@nestjs/common';
-import { StudentInterface } from './interfaces/student.interface';
-import { StudentService } from './student.service';
-
-@Controller('student')
+import { Controller, Get, Post, Put, Patch, Param } from "@nestjs/common";
+import { StudentInterface } from "./interfaces/student.interface";
+import { StudentService } from "./student.service";
+import { ApiTags } from "@nestjs/swagger";
+@ApiTags("Student")
+@Controller("student")
 export class StudentController {
+  constructor(private service: StudentService) {}
 
-    constructor(private service: StudentService) {}
-
-    @Get(":id")
-    getOne(@Param('id') id): any {
-      return this.service.getOne(id)
-    }
-    
-    
+  @Get(":id")
+  getOne(@Param("id") id): any {
+    return this.service.getOne(id);
+  }
 }
