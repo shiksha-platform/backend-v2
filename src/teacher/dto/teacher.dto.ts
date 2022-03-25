603607ba1ecbed8a75b9665b363bf8ed70d293a7
ManojNathIC <manojnath164@gmail.com>
2022-03-25 17:31:41 +0530

    Added get student API

---
+++ src/teacher/dto/teacher.dto.ts
@@ -0,0 +1,206 @@
+import { Exclude, Expose } from "class-transformer";
+import {
+  MaxLength,
+  IsNotEmpty,
+  IsEmail,
+  IsString,
+  IsNumber,
+} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class TeacherDto {
+  @Expose()
+  teacherId: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The firstname of the teacher",
+  })
+  @Expose()
+  firstName: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The lastname of the teacher",
+  })
+  @Expose()
+  lastName: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The contact number of the teacher",
+  })
+  @IsNumber()
+  contactNumber: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The email of the teacher",
+  })
+  @Expose()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The gender of the teacher",
+  })
+  @Expose()
+  gender: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The socialCategory of the teacher",
+  })
+  @Expose()
+  socialCategory: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The birthDate of the teacher",
+  })
+  @Expose()
+  birthDate: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The designation of the teacher",
+  })
+  @Expose()
+  designation: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The cadre of the teacher",
+  })
+  @Expose()
+  cadre: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The profQualification of the teacher",
+  })
+  @Expose()
+  profQualification: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The joiningDate of the teacher",
+  })
+  @Expose()
+  joiningDate: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The subjectId of the teacher",
+  })
+  @Expose()
+  subjectId: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The bloodGroup of the teacher",
+  })
+  @Expose()
+  bloodGroup: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The maritalStatus of the teacher",
+  })
+  @Expose()
+  maritalStatus: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The blockId of the teacher",
+  })
+  @Expose()
+  blockI: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The address of the teacher",
+  })
+  @Expose()
+  address: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The compSkills of the teacher",
+  })
+  @Expose()
+  compSkills: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The disability of the teacher",
+  })
+  @Expose()
+  disability: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The religion of the teacher",
+  })
+  @Expose()
+  religion: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The homeDistance of the teacher",
+  })
+  @Expose()
+  homeDistance: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The roles of the teacher",
+  })
+  @Expose()
+  roles: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The schoolId of the teacher",
+  })
+  @Expose()
+  schoolId: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The acrId of the teacher",
+  })
+  @Expose()
+  acrId: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The retirementDate of the teacher",
+  })
+  @Expose()
+  retirementDate: string;
+
+  @ApiProperty({
+    type: String,
+    description: "The workingStatus of the teacher",
+  })
+  @Expose()
+  workingStatus: string;
+
+  @Expose()
+  osCreatedAt: string;
+
+  @Expose()
+  osUpdatedAt: string;
+
+  @Expose()
+  osCreatedBy: string;
+
+  @Expose()
+  osUpdatedBy: string;
+
+  constructor(obj: TeacherDto) {
+    Object.assign(this, obj);
+  }
+}
--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -4,30 +4,71 @@ import { HttpService } from "@nestjs/axios";
 import { AxiosResponse } from "axios";
 import { first, map, Observable } from "rxjs";
 import { response } from "express";
-
+import { SuccessResponse } from "src/success-response";
+import { TeacherDto } from "./dto/teacher.dto";
 @Injectable()
 export class TeacherService {
   private teacher: TeacherInterface;
 
   constructor(private httpService: HttpService) {}
+  url = `${process.env.BASEAPIURL}/Teacher`;
 
-  getOne(id: any): Observable<TeacherInterface> {
-    return this.httpService
-      .get(
-        process.env.BASEAPIURL+"Teacher/"+id
-      )
-      .pipe(
-        map((axiosResponse: AxiosResponse) => {
-          this.teacher = {
-            teacherId: id,
-            firstName: axiosResponse.data.firstName,
-            lastName: axiosResponse.data.lastName,
-            email: axiosResponse.data.email,
-            contactNumber: axiosResponse.data.contactNumber
-          };
-          
-          return this.teacher;
-        })
-      );
+  getTeacher(id: any, request: any) {
+    return this.httpService.get(`${this.url}/${id}`, request).pipe(
+      map((axiosResponse: AxiosResponse) => {
+        let data = axiosResponse.data;
+        const teacher = {
+          teacherId: id,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          contactNumber: data.contactNumber,
+          email: data.email,
+          gender: data.gendar,
+          socialCategory: "",
+          birthDate: "",
+          designation: "",
+          cadre: "",
+          profQualification: "",
+          joiningDate: "",
+          subjectId: "",
+          bloodGroup: "",
+          maritalStatus: "",
+          blockI: "",
+          address: "",
+          compSkills: "",
+          disability: "",
+          religion: "",
+          homeDistance: "",
+          roles: "",
+          schoolId: "",
+          acrId: "",
+          retirementDate: "",
+          workingStatus: "",
+          osCreatedAt: data.osCreatedAt,
+          osUpdatedAt: data.osUpdatedAt,
+          osCreatedBy: data.osCreatedBy,
+          osUpdatedBy: data.osUpdatedBy,
+        };
+
+        const teacherDto = new TeacherDto(teacher);
+        return new SuccessResponse({
+          statusCode: 200,
+          message: "Student found Successfully",
+          data: teacherDto,
+        });
+      })
+    );
+  }
+
+  public async createTeacher(request: any, teacherDto: TeacherDto) {
+    return this.httpService.post(`${this.url}`, teacherDto, request).pipe(
+      map((axiosResponse: AxiosResponse) => {
+        return new SuccessResponse({
+          statusCode: 200,
+          message: "Student created Successfully",
+          data: axiosResponse.data,
+        });
+      })
+    );
   }
 }
