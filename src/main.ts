ea44f5afd6783cbcaade27955f700720736d22ef
Ashwin Date <coolbung@gmail.com>
2022-04-06 12:53:33 +0530

    Added api/v1 prefix to all routes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { RequestMethod } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
   const config = new DocumentBuilder()
-    .setTitle("Samagra")
+    .setTitle("Shiksha Platform")
     .setDescription("CRUD API")
     .setVersion("1.0")
-    .addTag("V2")
+    .addTag("V1")
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
