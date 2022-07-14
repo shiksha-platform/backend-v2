2be7770011b7b9607171ea3753ad8e899fd6ead2
TTPL-RT-50 <manojnath164@gmail.com>
2022-07-14 14:15:43 +0530

    assessmentset API adapter changes

--- src/adapters/sunbirdrc/assessment.adapter.ts
+++ src/adapters/sunbirdrc/assessment.adapter.ts
@@ -7,7 +7,6 @@ import { SuccessResponse } from "src/success-response";
 import { AssessmentDto } from "src/assessment/dto/assessment.dto";
 import { ErrorResponse } from "src/error-response";
 import { AssessmentSearchDto } from "src/assessment/dto/assessment-search-dto";
-import { AssessmentsetDto } from "src/assessment/dto/assessmentset.dto";
 @Injectable()
 export class AssessmentService {
   constructor(private httpService: HttpService) {}
@@ -93,84 +92,6 @@ export class AssessmentService {
         map((response) => {
           const responsedata = response.data.map(
             (item: any) => new AssessmentDto(item)
-          );
-          return new SuccessResponse({
-            statusCode: response.status,
-            message: "Ok.",
-            data: responsedata,
-          });
-        }),
-        catchError((e) => {
-          var error = new ErrorResponse({
-            errorCode: e.response.status,
-            errorMessage: e.response.data.params.errmsg,
-          });
-          throw new HttpException(error, e.response.status);
-        })
-      );
-  }
-
-  public async createAssessmentSet(
-    request: any,
-    assessmentsetDto: AssessmentsetDto
-  ) {
-    return this.httpService
-      .post(`${this.assessmentsetURL}`, assessmentsetDto, {
-        headers: {
-          Authorization: request.headers.authorization,
-        },
-      })
-      .pipe(
-        map((axiosResponse: AxiosResponse) => {
-          return new SuccessResponse({
-            statusCode: 200,
-            message: "Ok.",
-            data: axiosResponse.data,
-          });
-        }),
-        catchError((e) => {
-          var error = new ErrorResponse({
-            errorCode: e.response?.status,
-            errorMessage: e.response?.data?.params?.errmsg,
-          });
-          throw new HttpException(error, e.response.status);
-        })
-      );
-  }
-  public async getAssessmentset(assessmentsetId: any, request: any) {
-    return this.httpService
-      .get(`${this.assessmentsetURL}/${assessmentsetId}`, {
-        headers: {
-          Authorization: request.headers.authorization,
-        },
-      })
-      .pipe(
-        map((axiosResponse: AxiosResponse) => {
-          const data = axiosResponse.data;
-          const assessmentDto = new AssessmentsetDto(data);
-          return new SuccessResponse({
-            statusCode: 200,
-            message: "ok.",
-            data: assessmentDto,
-          });
-        })
-      );
-  }
-
-  public async searchAssessmentset(
-    request: any,
-    assessmentSearchDto: AssessmentSearchDto
-  ) {
-    return this.httpService
-      .post(`${this.assessmentsetURL}/search`, assessmentSearchDto, {
-        headers: {
-          Authorization: request.headers.authorization,
-        },
-      })
-      .pipe(
-        map((response) => {
-          const responsedata = response.data.map(
-            (item: any) => new AssessmentsetDto(item)
           );
           return new SuccessResponse({
             statusCode: response.status,
