61a79cafb2937e80ce220cc28ffb55fcbf29c86f
TTPL-LT-45 <snehalpatil0904>
2022-09-30 18:10:18 +0530

    course changes

--- src/adapters/diksha/dikshaCourse.adapter.ts
+++ src/adapters/diksha/dikshaCourse.adapter.ts
@@ -91,7 +91,7 @@ export class DikshaCourseService implements IServicelocator {
   public async getCoursesByIds(courseIds: [string], request: any) {
     let courseArray = [];
     for (let value of courseIds) {
-      let courseData = this.getCourseHierarchy(value);
+      let courseData = this.getCourseContent(value);
       courseArray.push(await courseData);
     }
     return new SuccessResponse({
@@ -103,7 +103,7 @@ export class DikshaCourseService implements IServicelocator {
 
   public async getCourseDetail(courseId: string, request: any) {
     let value = courseId;
-    let courseData = await this.getCourseHierarchy(value);
+    let courseData = await this.getCourseContent(value);
     return new SuccessResponse({
       statusCode: 200,
       message: "ok",
